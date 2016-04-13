f67681dad77751c914d122d8bbdad99e01efbfac
Frédéric Delorme <frederic.delorme@gmail.com>
2016-04-14 01:37:18 +0200

    add travis-ci as continuous build system.

--- src/main/java/com/webcontext/game/framework/state/impl/GameStateManager.java
+++ src/main/java/com/webcontext/game/framework/state/impl/GameStateManager.java
@@ -104,8 +104,8 @@ public class GameStateManager implements GSM {
 			pauseState.update(delay);
 			return;
 		}
-		// if (activeStates.containsKey(currentState))
-		activeStates.get(currentState).update(delay);
+		if (activeStates.containsKey(currentState))
+			activeStates.get(currentState).update(delay);
 	}
 
 	/*
